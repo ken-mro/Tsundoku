9f546bdb67b3957e26468f9c80136525c1dbcc57
murao <murao@angelplayingcards.com>
2024-08-27 16:25:09 +0900

    Add to specify book color

--- Models/Book.cs
+++ Models/Book.cs
@@ -2,9 +2,18 @@
 
 public class Book
 {
+    private IEnumerable<Color> _colors = new List<Color>
+    {
+        Color.FromHex("#E29401"),
+        Color.FromHex("#639A2C"),
+        Color.FromHex("#BC161C"),
+        Color.FromHex("#1D4F77"),
+    };
+
     public required int Id { get; init; }
     public required DateTime RegistrationDate { get; init; }
     public string RegistrationDateString => RegistrationDate.ToString("dd MMM yyyy");
+    public Color Color => _colors.ElementAt(Id % 4);
     public required DateTime ReadDate { get; set; }
     public required string Isbn10 { get; init; }
     public string ImageUrl => $"https://images.amazon.com/images/P/{Isbn10}.jpg";

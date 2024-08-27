9bcf9203ff8f99e634c96cd79be7c9879ecb193d
murao <murao@angelplayingcards.com>
2024-08-27 16:25:24 +0900

    Update MainPage view and its function

--- ViewModels/MainPageViewModel.cs
+++ ViewModels/MainPageViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
-using System.Diagnostics.Metrics;
 using Tsundoku.Models;
 using Tsundoku.Repository;
 
@@ -31,13 +30,71 @@ public partial class MainPageViewModel : BaseViewModel
         IsRefreshing = false;
     }
 
+    [RelayCommand]
+    private async Task DeleteBookAsync(Book book)
+    {
+        try
+        {
+            IsBusy = true;
+            var result = await _bookInfoRepository.DeleteBookInfoAsync(book.Id);
+            if (result > 0)
+            {
+                await Shell.Current.CurrentPage.DisplayAlert("Completed", "This book is deleted from the stack.", "OK");
+            }
+            else
+            {
+                throw new Exception("Failed to delete this book from the stack.");
+            }
+
+            await Init();
+        }
+        catch (Exception ex)
+        {
+            await Shell.Current.DisplayAlert($"Error", ex.Message, "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    [RelayCommand]
+    private async Task ReadBookAsync(Book book)
+    {
+        try
+        {
+            IsBusy = true;
+            var isCompleted = await Shell.Current.CurrentPage.DisplayAlert("Confirmation", "Have you read this book?", "Yes", "No");
+            if (!isCompleted) return;
+
+            var result = await _bookInfoRepository.UpdateReadStatusAsync(book.Id, DateTime.Now, true);
+            if (result > 0)
+            {
+                await Shell.Current.CurrentPage.DisplayAlert("Completed", "Certificate of your completion of this book issued.", "OK");
+            }
+            else
+            {
+                throw new Exception("Failed to issue certificate of your completion of this book.");
+            }
+            await Init();
+        }
+        catch (Exception ex)
+        {
+            await Shell.Current.DisplayAlert($"Error", ex.Message, "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
     private async Task Init()
     {
         try
         {
             IsBusy = true;
             var books = await _bookInfoRepository.GetAllBooksAsync();
-            BooksInStack = new ObservableCollection<Book>(books.Where(x => !x.Read).OrderByDescending(x =>x.Id).ToList());
+            BooksInStack = new ObservableCollection<Book>(books.Where(x => !x.Read).OrderBy(x =>x.Id).ToList());
         }
         catch (Exception ex)
         {

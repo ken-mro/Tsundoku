5fcf25f0297209ae8dceb2224549fdf294cba024
Ken Murao <murao.ken.36x@kyoto-u.jp>
2024-08-28 23:20:47 +0900

    Add font

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -19,6 +19,7 @@ namespace Tsundoku
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("craftmincho.otf", "craftmincho");
                 })
                 .UseBarcodeReader();
 

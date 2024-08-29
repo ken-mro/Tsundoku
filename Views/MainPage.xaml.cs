264b54243ae22c8cbb2222e44775a434b682d04d
Ken Murao <murao.ken.36x@kyoto-u.jp>
2024-08-29 22:14:16 +0900

    Delete unnecessary lines

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -4,16 +4,9 @@ namespace Tsundoku.Views;
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
-
     public MainPage(MainPageViewModel vm)
     {
         InitializeComponent();
         BindingContext = vm;
-    }
-
-    private void OnCounterClicked(object sender, EventArgs e)
-    {
-        count++;
     }
 }

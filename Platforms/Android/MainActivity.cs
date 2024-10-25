c9c3f8d0199a5ab185a9c25747e5f00b2ea7d6ef
Ken Murao <murao.ken.36x@kyoto-u.jp>
2024-10-26 02:05:37 +0900

    Update to be able to share and stack. (ios isn't confirmed.)

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,11 +1,41 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.Content;
 using Android.OS;
+using System.Text.RegularExpressions;
 
 namespace Tsundoku
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme", Exported = true, MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [IntentFilter([Intent.ActionSend], Categories = [Intent.CategoryDefault], DataMimeType = "text/plain")] // Add ****
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            TryToSaveSharedUrl(Intent!);
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+            TryToSaveSharedUrl(intent);
+        }
+
+        private static void TryToSaveSharedUrl(Intent intent)
+        {
+            if (intent?.Action == Intent.ActionSend && intent.Type == "text/plain")
+            {
+                string sharedText = intent.GetStringExtra(Intent.ExtraText) ?? string.Empty;
+                if (string.IsNullOrEmpty(sharedText)) return;
+
+                string urlPattern = @"https?://[^\s]+";
+                Match match = Regex.Match(sharedText, urlPattern);
+                if (!match.Success) return;
+
+                string url = match.Value;
+                MauiProgram.SharedData = url;
+            }
+        }
     }
 }
